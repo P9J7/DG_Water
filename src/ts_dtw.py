24028da2c109eb2e4c68628e3cb3c8beae7b06b1
Ynight <vae1986s>
2020-02-27 17:28:16 +0800

    usual

--- src/ts_dtw.py
+++ src/ts_dtw.py
@@ -11,6 +11,7 @@ from tslearn.datasets import CachedDatasets
 from tslearn.preprocessing import TimeSeriesScalerMeanVariance, \
     TimeSeriesResampler
 from tslearn.clustering import GlobalAlignmentKernelKMeans
+from tslearn.clustering import silhouette_score as ss
 from tslearn.clustering import KShape
 from tslearn.metrics import sigma_gak
 from tslearn.metrics import cdist_dtw
@@ -58,12 +59,23 @@ X_train = formatted_dataset
 X_train = TimeSeriesScalerMeanVariance().fit_transform(X_train)
 X_train = TimeSeriesResampler(sz=80).fit_transform(X_train)
 sz = X_train.shape[1]
-sdtw_km = TimeSeriesKMeans(n_clusters=4,
+best_score = 0.0
+best_n_cluster = None
+best_y_pred = None
+best_cluster_centers_ = None
+for i in np.arange(2, 7):
+    sdtw_km = TimeSeriesKMeans(n_clusters=i,
                            metric="softdtw",
                            metric_params={"gamma": .01},
                            verbose=True,
                            random_state=seed)
-y_pred = sdtw_km.fit_predict(X_train)
+    y_pred = sdtw_km.fit_predict(X_train)
+    score = ss(X_train, sdtw_km.labels_, metric='softdtw')
+    if score > best_score:
+        best_score = score
+        best_n_cluster = i
+        best_y_pred = y_pred
+        best_cluster_centers_ = sdtw_km.cluster_centers_
 # ks = KShape(n_clusters=5, verbose=True, random_state=seed)
 # y_pred = ks.fit_predict(X_train)
 # gak_km = GlobalAlignmentKernelKMeans(n_clusters=5,
@@ -72,13 +84,14 @@ y_pred = sdtw_km.fit_predict(X_train)
 #                                      verbose=True,
 #                                      random_state=seed)
 # y_pred = gak_km.fit_predict(X_train)
+print("聚%d"%best_n_cluster + "类的平均轮廓系数最高为：%f"%best_score)
 plt.figure(figsize=(20,10))
 # print(sdtw_km.labels_)
-for yi in range(4):
-    plt.subplot(4, 1,  1 + yi)
-    for xx in X_train[y_pred == yi]:
+for yi in range(best_n_cluster):
+    plt.subplot(best_n_cluster, 1,  1 + yi)
+    for xx in X_train[best_y_pred == yi]:
         plt.plot(xx.ravel(), "k-", alpha=.2)
-    plt.plot(sdtw_km.cluster_centers_[yi].ravel(), "r-")
+    plt.plot(best_cluster_centers_[yi].ravel(), "r-")
     plt.xlim(0, sz)
     # plt.ylim(-4, 4)
     plt.text(0.55, 0.85,'Cluster %d' % (yi + 1),
